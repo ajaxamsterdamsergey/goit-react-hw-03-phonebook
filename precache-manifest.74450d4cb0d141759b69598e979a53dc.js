65c129d302059ff9936bd51cd034ddfb3ae1eccd
S.Lazarenko <ajaxamsterdam878@gmail.com>
2020-03-24 21:31:38 +0200

    Updates

---
+++ precache-manifest.74450d4cb0d141759b69598e979a53dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e83e59ae529d94dc4afe7d854158ff",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "1c53afdba8d1b472eab3",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.d4484356.chunk.css"
+  },
+  {
+    "revision": "cd362d88b5672af4b8c7",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.ca24b494.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.ca24b494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c53afdba8d1b472eab3",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.75e6776f.chunk.js"
+  },
+  {
+    "revision": "32ca8000c2e5093c0bc4",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.fb1c9189.js"
+  }
+]);
